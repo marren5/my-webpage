2d698cd089871ab2066d263019fbf56500fb8656
marren5 <renstromm5@gmail.com>
2024-11-07 21:30:02 +0100

    commit changes

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 // script.js
 function toggleContent(index) {
     const content = document.getElementById(`content${index + 1}`);
+    // Toggle the display property between 'block' and 'none'
     if (content.style.display === "none" || content.style.display === "") {
-        content.style.display = "block";
+      content.style.display = "block";
     } else {
-        content.style.display = "none";
+      content.style.display = "none";
     }
-}
+  }
