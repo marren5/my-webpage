b4eab8e91e05a590039843bdbbc12214df217be0
marren5 <renstromm5@gmail.com>
2024-11-24 21:35:42 +0100

    for submission

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // script.js
 function toggleContent(index) {
     const content = document.getElementById(`content${index + 1}`);
-    // Toggle the display property between 'block' and 'none'
+    // Toggle the display property between 'block' and 'none' 
     if (content.style.display === "none" || content.style.display === "") {
       content.style.display = "block";
     } else {
